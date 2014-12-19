2dedd55baed22f44531347b82ba246400e0bce36
Lucas B Fronza <lbfronza@gmail.com>
2014-12-19 14:40:16 -0200

    visualizacao de perfil atualizado

--- application/views/profile/index.php
+++ application/views/profile/index.php
@@ -1,58 +1,76 @@
-        <div class="row">
-			<div class="col-md-offset-4 col-md-3">
-			
-			<div class="panel panel-default">
-			  <div class="panel-heading">
-				MINHA CONTA
-                  <div>
-                    <a href="<?=site_url('profile/edit')?>" class="btn btn-default" >Alterar dados</a>
-                  </div>
-			  </div>
-			  <div class="panel-body">
-				
-				<table class="table table-hover">
-						<tr>
-							<th style="text-align:right;"> Nome </th>
-							<td> <?php echo $user->name; ?> </td>
-						</tr>
-                        <tr>
-							<th style="text-align:right;"> Sobrenome </th>
-							<td> <?php echo $user->surname; ?> </td>
-						</tr>
-						<tr>
-							<th style="text-align:right;"> E-mail </th>
-							<td> <?php echo $user->email; ?> </td>
-						</tr>
-                        <tr>
-							<th style="text-align:right;"> Celular </th>
-							<td> <?php echo $user->phone; ?> </td>
-						</tr>
-                        <tr>
-							<th style="text-align:right;"> Endereço </th>
-							<td> <?php echo $user->address; ?> </td>
-						</tr>
-						<tr>
-							<th style="text-align:right;"> Cidade </th>
-							<td> <?php echo $user->city; ?> </td>
-						</tr>
-						<tr>
-							<th style="text-align:right;"> Estado </th>
-							<td> <?php echo $user->state; ?> </td>
-						</tr>
-						<tr>
-							<th style="text-align:right;"> Tipo </th>
-							<td> <?=ucfirst($user->type) ?> </td>
-						</tr>
-				</table>
-	
-				
-			  </div>
-			</div>
-			
-		  </div>
-		</div>
-			
-		<script src="<?=base_url('static/resources/jquery.min.js')?>"></script>
-	    <script src="<?=base_url('static/resources/bootstrap.min.js')?>"></script>
-	</body>
+		<div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                        	Perfil
+                        	<a href="<?=site_url('profile/edit')?>" class="btn btn-primary" >
+                        		<i class="fa fa-wrench fa-fw"></i>
+                        		Alterar dados
+                        	</a>
+                        </h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+                <div class="col-lg-6">
+                    <div class="table-responsive">
+                        <table class="table">
+	                        <tr>
+								<th style="text-align:left;"> Nome </th>
+								<td> <?php echo $user->name; ?> </td>
+							</tr>
+	                        <tr>
+								<th style="text-align:left;"> Sobrenome </th>
+								<td> <?php echo $user->surname; ?> </td>
+							</tr>
+							<tr>
+								<th style="text-align:left;"> E-mail </th>
+								<td> <?php echo $user->email; ?> </td>
+							</tr>
+	                        <tr>
+								<th style="text-align:left;"> Celular </th>
+								<td> <?php echo $user->phone; ?> </td>
+							</tr>
+	                        <tr>
+								<th style="text-align:left;"> Endereço </th>
+								<td> <?php echo $user->address; ?> </td>
+							</tr>
+							<tr>
+								<th style="text-align:left;"> Cidade </th>
+								<td> <?php echo $user->city; ?> </td>
+							</tr>
+							<tr>
+								<th style="text-align:left;"> Estado </th>
+								<td> <?php echo $user->state; ?> </td>
+							</tr>
+							<tr>
+								<th style="text-align:left;"> Tipo </th>
+								<td> <?=ucfirst($user->type) ?> </td>
+							</tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?=base_url('static/js/jquery.js')?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?=base_url('static/js/bootstrap.min.js')?>"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?=base_url('static/js/plugins/metisMenu/metisMenu.min.js')?>"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?=base_url('static/js/sb-admin-2.js')?>"></script>
+
+</body>
+
 </html>
--- application/views/profile/user.php
+++ application/views/profile/user.php
@@ -1,46 +1,65 @@
-        <div class="row">
-			<div class="col-md-offset-4 col-md-3">
-			
-			<div class="panel panel-default">
-			  <div class="panel-heading">
-				USUÁRIO
-                  <div>
-                    <a href="<?=site_url('messages/user/'.$user->id)?>" class="btn btn-default" >Mensagem</a>
-                  </div>
-			  </div>
-			  <div class="panel-body">
-				
-				<table class="table table-hover">
-						<tr>
-							<th style="text-align:right;"> Nome </th>
-							<td> <?php echo $user->name; ?> </td>
-						</tr>
-                        <tr>
-							<th style="text-align:right;"> Sobrenome </th>
-							<td> <?php echo $user->surname; ?> </td>
-						</tr>
-						<tr>
-							<th style="text-align:right;"> E-mail </th>
-							<td> <?php echo $user->email; ?> </td>
-						</tr>
-                        <tr>
-							<th style="text-align:right;"> Celular </th>
-							<td> <?php echo $user->phone; ?> </td>
-						</tr>
-						<tr>
-							<th style="text-align:right;"> Tipo </th>
-							<td> <?=ucfirst($user->type) ?> </td>
-						</tr>
-				</table>
-	
-				
-			  </div>
-			</div>
-			
-		  </div>
-		</div>
-			
-		<script src="<?=base_url('static/resources/jquery.min.js')?>"></script>
-	    <script src="<?=base_url('static/resources/bootstrap.min.js')?>"></script>
-	</body>
+		<div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                        	Usuário
+                        	<a href="<?=site_url('messages/user/'.$user->id)?>" class="btn btn-primary" >
+                        		<i class="fa fa-envelope fa-fw"></i>
+                        		Mensagem
+                        	</a>
+                        </h1>
+                        
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+                <div class="col-lg-6">
+                    <div class="table-responsive">
+                        <table class="table">
+		                    <tr>
+								<th style="text-align:left;"> Nome </th>
+								<td> <?php echo $user->name; ?> </td>
+							</tr>
+	                        <tr>
+								<th style="text-align:left;"> Sobrenome </th>
+								<td> <?php echo $user->surname; ?> </td>
+							</tr>
+							<tr>
+								<th style="text-align:left;"> E-mail </th>
+								<td> <?php echo $user->email; ?> </td>
+							</tr>
+	                        <tr>
+								<th style="text-align:left;"> Celular </th>
+								<td> <?php echo $user->phone; ?> </td>
+							</tr>
+							<tr>
+								<th style="text-align:left;"> Tipo </th>
+								<td> <?=ucfirst($user->type) ?> </td>
+							</tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?=base_url('static/js/jquery.js')?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?=base_url('static/js/bootstrap.min.js')?>"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?=base_url('static/js/plugins/metisMenu/metisMenu.min.js')?>"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?=base_url('static/js/sb-admin-2.js')?>"></script>
+
+</body>
+
 </html>
