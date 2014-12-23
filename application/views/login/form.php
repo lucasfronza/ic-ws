47df1b3561850178fdfaf63b63911222eff3f3f3
Lucas B Fronza <lbfronza@gmail.com>
2014-12-23 01:45:50 -0200

    layout login atualizado

--- application/views/login/form.php
+++ application/views/login/form.php
@@ -1,60 +1,70 @@
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                	<h1 class="page-header">Login</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-4">
+					<form action="<?=site_url('login/authenticate')?>" method="post" role="form" id="form-login" onsubmit="return validate_login();">
+						  
+				        <div class="form-group" id="groupEmail">
+				            <label>Email</label>
+				            <input type="text" class="form-control" id="email" name="email">
+				            <p id="errorEmail" class="help-block"></p>
+				        </div>
+			                
+				        <div class="form-group" id="groupPassword">
+				            <label>Senha</label>
+				            <input type="password" class="form-control" id="password" name="password">
+				        </div> 
+			        
+			        	<hr>
 
-	
-	<div class="row">
-		<div class="col-md-offset-3 col-md-6">
-		
-		<div class="panel panel-default">
-		  <!-- <div class="panel-heading">
-			   <h3 class="panel-title">LOGIN</h3>
-		  </div> -->
-		  <div class="panel-body">
-			<!-- <legend class="text-center">Welcome to ICMC's Mobile Learning Environment</legend> -->
-		  	<img src="<?=base_url('static/img/logo_icmc_usp_mle.png')?>" class="img-responsive" alt="Responsive image">
-			<legend></legend>
-			<form action="<?=site_url('login/authenticate')?>" method="post" class="form-horizontal" role="form" id="form-login" onsubmit="return validate_login();">
-			  
-        <div class="form-group" id="groupEmail">
-            <label for="email" class="col-sm-2 control-label">Email</label>
-            <div class="col-sm-5">
-                <input type="text" class="form-control" id="email" name="email">
+						<div class="form-group">
+							<button type="submit" class="btn btn-primary">Entrar</button>
+							<a href="<?=site_url('login/password_recovery')?>" class="btn btn-default">Esqueci a senha</a>
+						</div>
+
+					</form>
+					<?php if (!@$user_profile): ?>
+						<div class="form-group" style="width: 100px">
+					  		<a class="btn btn-block btn-social btn-facebook" href="<?= $login_url ?>" rel="nofollow">
+								<i class="fa fa-facebook"></i>
+								Login
+					        </a>
+				        </div>
+			    	<?php endif ?>
+                </div>
+                <!-- /.col-lg-12 -->
             </div>
-            <div class="col-sm-4">
-                <span id="errorEmail" class="help-block"></span>
-            </div>
+            <!-- /.row -->
         </div>
-                
-        <div class="form-group" id="groupPassword">
-            <label for="password" class="col-sm-2 control-label">Senha</label>
-            <div class="col-sm-5">
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-        </div> 
-        
-        <hr>
-			  <div class="form-group">
-				  <div class="col-sm-offset-2 col-sm-5">
-				    <button type="submit" class="btn btn-primary">Entrar</button>
-                    <a href="<?=site_url('login/password_recovery')?>" class="btn btn-default">Esqueci a senha</a>
-				  </div>
-			  </div>
-			</form>
-			<?php if (!@$user_profile): ?>
-				<div class="form-group col-sm-offset-2" style="width: 100px">
-			  		<a class="btn btn-block btn-social btn-facebook" href="<?= $login_url ?>" rel="nofollow">
-			          <i class="fa fa-facebook"></i>
-			          Login
-			        </a>
-		        </div>
-	    	<?php endif ?>
-		  </div>
-		</div>
-		
-	  </div>
-	</div>
-		
-    <script src="<?=base_url('static/resources/jquery.min.js')?>"></script>
-    <script src="<?=base_url('static/resources/bootstrap.min.js')?>"></script>
-    <script src="<?=base_url('static/resources/validation.js')?>"></script>
-  
+        <!-- /#page-wrapper -->
 
-</body></html>
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Social Buttons CSS -->
+    <link href="<?=base_url('static/css/plugins/social-buttons.css')?>" rel="stylesheet">
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="<?=base_url('static/js/jquery-1.11.0.js')?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?=base_url('static/js/bootstrap.min.js')?>"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?=base_url('static/js/plugins/metisMenu/metisMenu.min.js')?>"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?=base_url('static/js/sb-admin-2.js')?>"></script>
+
+    <!-- Form validation -->
+    <script src="<?=base_url('static/resources/validation.js')?>"></script>
+
+</body>
+
+</html>
