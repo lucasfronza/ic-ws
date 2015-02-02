946945368c0b9b887b5a6f802e3c3dcdb8409ca8
Lucas B Fronza <lbfronza@gmail.com>
2015-02-02 10:19:12 -0200

    correção layout da busca de usuários

--- application/views/course/users_search.php
+++ application/views/course/users_search.php
@@ -1,76 +1,92 @@
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Resultado da busca
+                            <a href="<?=site_url('course/manage/'.$idCourse)?>" class="btn btn-warning">Voltar</a>
+                        </h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+                <div class="col-lg-8">
+                    <?php if (empty($search)): ?>
+                      <p style="text-align:center;">Nenhum usuário encontrado.</p>
+                      <?php else: ?>
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th> NOME COMPLETO </th>
+                                        <th> EMAIL </th>
+                                        <th> ADICIONAR </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($search as $item): ?>
+                                    <tr>
+                                        <td><?=$item->name?> <?=$item->surname?></td>
+                                        <td><?=$item->email?></td>
+                                        <td><a class="btn btn-primary" href="<?=site_url('course/addUser/'.$idCourse.'/'.$item->id)?>"><span class="glyphicon glyphicon-ok"></span></a></td>
+                                    </tr>
+                                    <?php endforeach ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Participantes
+                        </h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /.row -->
+                <div class="col-lg-8">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th> NOME COMPLETO </th>
+                                    <th> EMAIL </th>
+                                    <th> REMOVER </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($linkedUsers as $item): ?>
+                                <tr>
+                                    <td><?=$item->name?> <?=$item->surname?></td>
+                                    <td><?=$item->email?></td>
+                                    <td><a class="btn btn-danger" href="<?=site_url('course/removeUser/'.$idCourse.'/'.$item->id)?>"><span class="glyphicon glyphicon-remove"></span></a></td>
+                                </tr>
+                                <?php endforeach ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
 
-		
-		<div class="row">
-			<div class="col-md-offset-3 col-md-6">
-			
-			<div class="panel panel-default">
-			  <div class="panel-heading">
-				RESULTADO DA BUSCA
-			  </div>
-			  <div class="panel-body">
-				
-                  <?php if (empty($search)): ?>
-                  <p style="text-align:center;">Nenhum usuário encontrado.</p>
-                  <?php else: ?>
+    </div>
+    <!-- /#wrapper -->
 
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th> NOME COMPLETO </th>
-                                <th> EMAIL </th>
-                                <th> ADICIONAR </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($search as $item): ?>
-                            <tr>
-                                <td><?=$item->name?> <?=$item->surname?></td>
-                                <td><?=$item->email?></td>
-                                <td><a class="btn btn-primary" href="<?=site_url('course/addUser/'.$idCourse.'/'.$item->id)?>"><span class="glyphicon glyphicon-ok"></span></a></td>
-                            </tr>
-                            <?php endforeach ?>
-                        </tbody>
-                    </table>
-                <?php endif; ?>
-                  
-                  
-			  </div>
-			</div>
-                
-            <div class="panel panel-default">
-			  <div class="panel-heading">
-				PARTICIPANTES
-			  </div>
-			  <div class="panel-body">
-				
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th> NOME COMPLETO </th>
-                                <th> EMAIL </th>
-                                <th> REMOVER </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($linkedUsers as $item): ?>
-                            <tr>
-                                <td><?=$item->name?> <?=$item->surname?></td>
-                                <td><?=$item->email?></td>
-                                <td><a class="btn btn-danger" href="<?=site_url('course/removeUser/'.$idCourse.'/'.$item->id)?>"><span class="glyphicon glyphicon-remove"></span></a></td>
-                            </tr>
-                            <?php endforeach ?>
-                        </tbody>
-                    </table>
-                  
-			  </div>
-			</div>
-			
-		  </div>
-		</div>
-			
-		<script src="<?=base_url('static/resources/jquery.min.js')?>"></script>
-    	<script src="<?=base_url('static/resources/bootstrap.min.js')?>"></script>
-    	
-  
-	</body>
+    <!-- jQuery -->
+    <script src="<?=base_url('static/js/jquery-1.11.0.js')?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?=base_url('static/js/bootstrap.min.js')?>"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?=base_url('static/js/plugins/metisMenu/metisMenu.min.js')?>"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?=base_url('static/js/sb-admin-2.js')?>"></script>
+
+</body>
+
 </html>
