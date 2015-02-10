a0244ab2449482171fb47c2977fa548c9ce5381d
Lucas B Fronza <lbfronza@gmail.com>
2015-02-10 10:29:25 -0200

    configuração para tela cheia em mobile

--- application/views/main/header_menu.php
+++ application/views/main/header_menu.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
 
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
 
     <title><?=$title?></title>
 
